961f73b0218965aaacdb1cc0d7a3e080f0c254b2
Mark Reynolds <mark.reynolds@circonus.com>
2016-02-04 22:25:53 +0000

    minor typo

--- fassert.h
+++ fassert.h
@@ -7,8 +7,8 @@
 #include <string.h>
 #include <strings.h>
 
-extern void fassertx(bool tf, int ln, const char *fn, const char *str);
-extern void fassertxsetpath(const char *path);
+extern void  fassertx(bool tf, int ln, const char *fn, const char *str);
+extern void  fassertxsetpath(const char *path);
 extern const char *fassertxgetpath(void);
 
 #define FASSERT(A,B)  { bool tf = (A); fassertx(tf, __LINE__, __FILE__, \
