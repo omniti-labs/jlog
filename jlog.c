251ba86b4aeb81de010ad304658deeb1032ff1b8
Philip Maddox <philip.maddox@circonus.com>
2024-05-28 17:13:50 +0000

    Remove unneeded breaks

--- jlog.c
+++ jlog.c
@@ -985,7 +985,6 @@ static int __jlog_setup_reader(jlog_ctx *ctx, u_int32_t log, u_int8_t force_mmap
       }
       ctx->reader_is_initialized = 1;
       return 0;
-      break;
     case JLOG_READ_METHOD_PREAD:
       if (force_mmap) {
         int rv = __jlog_mmap_reader(ctx, log);
@@ -1002,7 +1001,6 @@ static int __jlog_setup_reader(jlog_ctx *ctx, u_int32_t log, u_int8_t force_mmap
       }
       ctx->reader_is_initialized = 1;
       return 0;
-      break;
     default:
      SYS_FAIL(JLOG_ERR_NOT_SUPPORTED);
      break;
